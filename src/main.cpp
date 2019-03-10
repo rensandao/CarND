aed3fe73d781352c36aacebb2b4b3f7cd0326e79
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-10 10:30:24 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -139,39 +139,39 @@ int main() {
 		}  // end websocket message if
 	}); // end h.onMessage
 
-  // We don't need this since we're not using HTTP but if it's removed the program
-  // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-    const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1)
-    {
-      res->end(s.data(), s.length());
-    }
-    else
-    {
-      // i guess this should be done more gracefully?
-      res->end(nullptr, 0);
-    }
-  });
+	// We don't need this since we're not using HTTP but if it's removed the program
+	// doesn't compile :-(
+	h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+		const std::string s = "<h1>Hello world!</h1>";
+		if (req.getUrl().valueLength == 1)
+		{
+		  res->end(s.data(), s.length());
+		}
+		else
+		{
+		  // i guess this should be done more gracefully?
+		  res->end(nullptr, 0);
+		}
+	});
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+		std::cout << "Connected!!!" << std::endl;
+	});
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+		ws.close();
+		std::cout << "Disconnected" << std::endl;
+	});
 
-  int port = 4567;
-  if (h.listen(port))
-  {
-    std::cout << "Listening to port " << port << std::endl;
-  }
-  else
-  {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  h.run();
+	int port = 4567;
+	if (h.listen(port))
+	{
+		std::cout << "Listening to port " << port << std::endl;
+	}
+	else
+	{
+		std::cerr << "Failed to listen to port" << std::endl;
+		return -1;
+	}
+	h.run();
 }
